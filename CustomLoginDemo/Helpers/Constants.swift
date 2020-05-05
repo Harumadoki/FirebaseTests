893174b4fcb9935f41e5b9914c93f0572535a95f
p1612150 <heritiana.rasoanaivo@etu.univ-lyon1.fr>
2020-05-05 21:34:51 +0200

    authentification réussi avec rajout d'utilisateur

---
+++ CustomLoginDemo/Helpers/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  CustomLoginDemo
+//
+//  Created by Heritiana RASOANAIVO on 05/05/2020.
+//  Copyright © 2020 Heritiana RASOANAIVO. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+}
